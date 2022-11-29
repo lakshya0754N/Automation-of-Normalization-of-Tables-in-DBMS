805e83b141ad3073c8ec85cab89b138482a44b67
Lakshya Kumawat <lakshyakumawat312002@gmail.com>
2022-11-29 17:49:20 +0530

    final commit

--- app.py
+++ app.py
@@ -2,10 +2,47 @@ import streamlit as st
 import pandas as pd
 from table import Table
 from autoNorm import AutoNormalization
+import mysql.connector
+
+conn=mysql.connector.connect(host='localhost',port=3306,user='lakshya0754n', password='lakshya',database='dbTest')
+
+def insertinSQL(tableName, df):
+    Attr=df.columns
+    cursor = conn.cursor()
+    dataType=list(df.dtypes)
+    arr=[]
+    for i in dataType:
+        if i=='int64':
+            arr.append('int')
+        elif i=='float64':
+            arr.append('float')
+        elif i=='O':
+            arr.append('varchar(255)')
+        elif i=='bool':
+            arr.append('bool')
+    
+    variables=""
+    for i in range(len(Attr)):
+        variables+=Attr[i]+" " + str(arr[i]) + ","
+    variables = variables.rstrip(variables[-1])
+    query="CREATE TABLE "+tableName+" (" + variables+");"
+    df=df.values
+    cursor.execute(query)
+    for row in df:
+        row=tuple(row)
+        s=""
+        for j in row:
+            s+="%s,"
+        s=s.rstrip(s[-1])
+    
+        q="Insert into "+tableName+" Values ("+s+")"
+        cursor.execute(q, row)
+        conn.commit()
+
 
 st.set_page_config(page_title="auto-normalization-in-dbms", page_icon=":bar_chart:")
 
-st.subheader("Hi! I am Tushar")
+st.subheader("AutoNorm")
 st.title("Auto Normalization of the table: ")
 
 tableName = st.text_input("Enter the name of the table: ")
@@ -40,6 +77,7 @@ st.subheader("Normalized Tables(BCNF): \n")
     
 for i,tbl in enumerate(normalized.data):
     st.write(f"Table{i+1} : {tbl.tableName}")
+    insertinSQL(normalized.data[i].tableName,normalized.data[i].table)
     st.write(tbl.table)
     pKey = ""
     for attr in tbl.primaryKeys:
@@ -47,3 +85,4 @@ for i,tbl in enumerate(normalized.data):
     st.write(f"Primary Key: {pKey}")
     print(tbl.primaryKeys)
     
+conn.close()
